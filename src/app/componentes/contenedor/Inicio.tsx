948c1f31a96e49323ecaff269430aafd38e3b216
jmoscote10 <jmoscote10@cuc.edu.co>
2024-11-12 23:15:29 -0500

    Inicio libro completo

--- src/app/componentes/contenedor/Inicio.tsx
+++ src/app/componentes/contenedor/Inicio.tsx
@@ -1,9 +1,168 @@
-export const Inicio = ()=>{
-    return(
-        <>
-        <br /><br />
-        Soy el Inico<br/>
-        Como el de la bienvenida...
-        </>
-    )
-}
+import { NavLink } from "react-router-dom";
+export const Inicio = () => {
+  return (
+    <>
+      <br />
+      <br />
+      <div>
+        <section className="py-5 text-center container">
+          <div className="row py-lg-5">
+            <div className="col-lg-6 col-md-8 mx-auto">
+              <h1 className="fw-light">Colección de Libros</h1>
+              <p className="lead text-body-secondary">
+                Explora nuestra colección de libros cuidadosamente seleccionados: 
+                una variedad de géneros y temas para todos los gustos y edades.
+                Cada libro cuenta una historia única, esperando ser descubierta.
+              </p>
+              <p>
+                <NavLink to="*" className="btn btn-primary my-2">
+                 Explorar Libros
+                </NavLink>
+                <NavLink to="/libcre" className="btn btn-secondary my-2">
+                 Agregar Nuevo Libro
+                </NavLink>
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <div className="album py-5 bg-body-tertiary">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Portada</title>
+                    <rect width="100%" height="100%" fill="#55595c"></rect>
+                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                      Portada del libro
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                     Explora este libro que ofrece una historia apasionante. 
+                     Descubre detalles únicos y déjate cautivar por cada página.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Ver
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Editar
+                        </button>
+                      </div>
+                      <small className="text-body-secondary">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Portada</title>
+                    <rect width="100%" height="100%" fill="#55595c"></rect>
+                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                     Portada del libro
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                     Sumérgete en otra obra fascinante que promete llevarte a 
+                     un mundo nuevo, lleno de intriga y emoción.
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Ver
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Editar
+                        </button>
+                      </div>
+                      <small className="text-body-secondary">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col">
+                <div className="card shadow-sm">
+                  <svg
+                    className="bd-placeholder-img card-img-top"
+                    width="100%"
+                    height="225"
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    aria-label="Placeholder: Thumbnail"
+                    preserveAspectRatio="xMidYMid slice"
+                    focusable="false"
+                  >
+                    <title>Portada</title>
+                    <rect width="100%" height="100%" fill="#55595c"></rect>
+                    <text x="50%" y="50%" fill="#eceeef" dy=".3em">
+                     Portada del libro
+                    </text>
+                  </svg>
+                  <div className="card-body">
+                    <p className="card-text">
+                     Un libro ideal para aquellos que buscan aventuras
+                     y experiencias únicas en cada capítulo.
+                  
+                    </p>
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="btn-group">
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Ver
+                        </button>
+                        <button
+                          type="button"
+                          className="btn btn-sm btn-outline-secondary"
+                        >
+                          Editar
+                        </button>
+                      </div>
+                      <small className="text-body-secondary">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
